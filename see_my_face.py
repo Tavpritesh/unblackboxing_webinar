22cac1312d5227144dfd30d752840fb9b26eacfa
Jakub Czakon <jakub.czakon@pascal01.intra.codilime.com>
2017-09-25 21:01:20 +0200

    dropped ipynb

--- see_my_face.py
+++ see_my_face.py
@@ -1,16 +1,22 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+import tensorflow as tf
 from sklearn.datasets import fetch_lfw_people
 
 from facial_recognition.preprocessing import lfw_train_test_split, FacePreprocessor, tensor2img, img2tensor
 
-NEPTUNE = True
+
+NEPTUNE = False
 if NEPTUNE:
-    DATA_DIR = '/mnt/ml-team/homes/jakub.czakon/.data/scikit_learn_data'
-    MODEL_FILEPATH = '/mnt/ml-team/homes/jakub.czakon/.unblackboxing_webinar_data/models/facenet.h5py'
+    DATA_DIR = 'input/scikit_learn_data'
+    MODEL_FILEPATH = 'input/models/facenet.h5py'
     from facial_recognition.model import FaceClassifierNeptune as FaceClassifier
 
 else:
-    DATA_DIR = 'input/scikit_learn_data'
-    MODEL_FILEPATH = 'input/models/facenet.h5py'
+    DATA_DIR = '/mnt/ml-team/homes/jakub.czakon/.data/scikit_learn_data'
+    MODEL_FILEPATH = '/mnt/ml-team/homes/jakub.czakon/.unblackboxing_webinar_data/models/facenet.h5py'
     from facial_recognition.model import FaceClassifier
 
 
@@ -25,8 +31,10 @@ if __name__ == '__main__':
     X_train, y_train = face_prep.fit_transform(X=X_train, y=y_train)
     X_test, y_test = face_prep.transform(X=X_test, y=y_test)
     
-    print('Data preprocessed')
-    face_classifier = FaceClassifier(input_shape=(125, 94, 3), classes=5, 
-                                     model_save_filepath=MODEL_FILEPATH)
+    with tf.device('/gpu:1'):
 
-    face_classifier.train((X_train, y_train), (X_test,y_test), batch_size=8, epochs=30)
+        print('Data preprocessed')
+        face_classifier = FaceClassifier(input_shape=(125, 94, 3), classes=5, 
+                                         model_save_filepath=MODEL_FILEPATH)
+
+        face_classifier.train((X_train, y_train), (X_test,y_test), batch_size=8, epochs=30)
